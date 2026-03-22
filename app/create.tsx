100cb7e77768f24c329cb787657a6f93ec8a28e3
balajiNadar08 <balajigittttt@gmail.com>
2026-03-22 21:37:56 +0530

    Testing

--- app/create.tsx
+++ app/create.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import card1 from "../assets/images/card1.png";
 
 const create = () => {
   return (
-    <View>
-      <Text>CREATE CARD PAGE</Text>
+    <View className="bg-blue-400 flex-1 items-center justify-center">
+      <Text className="text-white mb-4">CREATE CARD PAGE</Text>
+      <Image
+        source={card1}
+        className="w-64 h-96"
+        resizeMode="contain"
+      />
     </View>
-  )
-}
+  );
+};
 
 export default create;
