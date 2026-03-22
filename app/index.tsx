246cc074e1373968d6ad57a3a4216816130b91fd
balajiNadar08 <balajigittttt@gmail.com>
2026-03-22 21:38:15 +0530

    Minor UI changes

--- app/index.tsx
+++ app/index.tsx
@@ -33,6 +33,20 @@ export default function Index() {
         PookieMon Cards
       </Text>
 
+      <Text
+        style={{ fontFamily: "BubblegumSans_400Regular" }}
+        className="text-white text-base text-center opacity-90"
+      >
+        Create your own legendary cards 
+      </Text>
+
+      <Text
+        style={{ fontFamily: "BubblegumSans_400Regular" }}
+        className="text-white text-sm text-center opacity-70 px-6"
+      >
+        Upload images, customize abilities, and build your own PookieMon card
+      </Text>
+
       <Link href="/create" asChild>
         <Pressable className="mt-6 px-6 py-3 rounded-full bg-white active:scale-95">
           <Text
@@ -43,6 +57,8 @@ export default function Index() {
           </Text>
         </Pressable>
       </Link>
+
+      
     </View>
   );
 }
